298c5c3a2ba94e40b75996123b2dd935cce42863
Kotone Fami <fami@ktnfm.com>
2024-02-12 12:33:15 +0900

    Changed indents

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -1,13 +1,13 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore({
-    id: 'counter',
+    id: "counter",
     state: () => ({
         count: 0
     }),
     actions: {
         increment() {
-            this.count++
+            this.count++;
         }
     }
-})
+});
