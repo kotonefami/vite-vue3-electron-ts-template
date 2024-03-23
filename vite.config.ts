94b1d88d70fe889819b32e1648a9253a363ffefc
Kotone Fami <fami@ktnfm.com>
2024-03-23 12:42:24 +0900

    Changed indent and quotes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  root: './src/frontend/',
-  base: './',
-  server: {
-    hmr: {
-      overlay: false
+    plugins: [vue()],
+    root: "./src/frontend/",
+    base: "./",
+    server: {
+        hmr: {
+            overlay: false
+        }
     }
-  }
 })
