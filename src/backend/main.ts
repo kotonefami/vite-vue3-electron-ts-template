03be4f7535ae4739f45f57d4d33a4376532cec9e
Kotone Fami <fami@ktnfm.com>
2024-03-23 12:49:03 +0900

    Removed comments

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -16,7 +16,6 @@ async function handleFileOpen() {
 }
 
 function createWindow() {
-    // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -25,36 +24,21 @@ function createWindow() {
         },
     });
 
-    // and load the index.html of the app.
     if (isDev) {
-        mainWindow.loadURL('http://localhost:3000');// Open the DevTools.
-        mainWindow.webContents.openDevTools();
+        mainWindow.loadURL('http://localhost:3000');
     } else {
         mainWindow.loadFile(join(__dirname, '../frontend/index.html'));
     }
-    // mainWindow.loadURL( //this doesn't work on macOS in build and preview mode
-    //     isDev ?
-    //     'http://localhost:3000' :
-    //     join(__dirname, '../frontend/index.html')
-    // );
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     ipcMain.handle('dialog:openFile', handleFileOpen)
     createWindow()
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
